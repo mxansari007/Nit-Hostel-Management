7b53809f0bd11448fdf1ec04029763ca87739eb2
Maaz Ansari <123573035+mxansari007@users.noreply.github.com>
2023-05-03 00:46:44 +0530

    set up database

--- backend/Server.mjs
+++ backend/Server.mjs
@@ -1,9 +1,14 @@
 // These lines make "require" available
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
+import { initializeApp } from "firebase/app";
+import { getFirestore,collection,doc,setDoc } from "firebase/firestore";
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+
+
+
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -24,6 +29,11 @@ const firebaseConfig = {
 const fapp = initializeApp(firebaseConfig);
 
 
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(fapp);
+const usersCollectionRef = collection(db, 'users');
+
+
 const express = require('express');
 const bp = require('body-parser');
 const path = require('path');
@@ -33,14 +43,21 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.json());
 
 app.use(bp.urlencoded({extended:true}));
+app.use(bp.json());
 
-app.post('',function(req,res){
-        console.log(req);
+app.post('/store-data',(req,res)=>{
+  console.log(req.body);   
+
+const DocumentRef = doc(db, 'users',req.body.rollno);   
+setDoc(DocumentRef,req.body);
 })
+
 
 
 app.listen(3000,function(){
     console.log("Server is listening on port 3000");
 })
+
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import './Signup.css';
+import axios from 'axios';
+
 
 export class Signup extends Component {
 
@@ -17,9 +19,29 @@ constructor(props){
   }
 }
 
+handleChange = (event) =>{
+  this.setState({[event.target.name]:event.target.value});
+}
 
-handleSubmit = (event)=>{
-  
+
+handleSubmit = (e)=>{
+  alert('A form was submitted: ' + this.state);
+ 
+      axios.post('http://localhost:3000/store-data',{
+          rollno: this.state.rollno,
+          firstName:this.state.firstName,
+          lastName:this.state.lastName,
+          mobileNumber: this.state.mobileNumber,
+          department:this.state.department,
+          year:this.state.year,
+          password:this.state.password,
+          confirmPass:this.state.confirmPass
+      }).then(response=>{
+        console.log(response);
+      }).catch(error=>{
+        console.log(error);
+      })
+      e.preventDefault();
 }
 
 
@@ -35,25 +57,25 @@ handleSubmit = (event)=>{
         <div className="form-group">
             
           <label for="rollno">Roll Number</label>
-          <input type="number" className="form-control" id="Rollno" name="rollno" placeholder="Enter Your Roll Number"/>
+          <input onChange={this.handleChange} type="number" className="form-control" id="Rollno" name="rollno" placeholder="Enter Your Roll Number" value={this.state.value}/>
           
         </div>
         <div className="form-group">
             
             <label for="firstName">First Name</label>
-            <input type="text" className="form-control" id="firstName" name="firstName" placeholder="Enter Your First Name"/>
+            <input onChange={this.handleChange} value={this.state.value} type="text" className="form-control" id="firstName" name="firstName" placeholder="Enter Your First Name"/>
             
           </div>
           <div className="form-group">
             
             <label for="firstName">Last Name</label>
-            <input type="text" className="form-control" id="lastName" name="lastName" placeholder="Enter Your Last Name"/>
+            <input onChange={this.handleChange} value={this.state.value} type="text" className="form-control" id="lastName" name="lastName" placeholder="Enter Your Last Name"/>
             
           </div>
           <div className="form-group">
             
             <label for="mobileNumber">Mobile Number</label>
-            <input type="number" className="form-control" id="mobileNumber" name="mobileNumber" placeholder="Enter Your Mobile Number"/>
+            <input onChange={this.handleChange} value={this.state.value} type="number" className="form-control" id="mobileNumber" name="mobileNumber" placeholder="Enter Your Mobile Number"/>
             
           </div>
         </div>
@@ -61,24 +83,24 @@ handleSubmit = (event)=>{
           <div className="form-group">
             
             <label for="department">Department</label>
-            <input type="text" className="form-control" id="department" name="department" placeholder="Enter Your Department"/>
+            <input onChange={this.handleChange} value={this.state.value} type="text" className="form-control" id="department" name="department" placeholder="Enter Your Department"/>
             
           </div>
           <div className="form-group">
             
             <label for="year">Year of Study</label>
-            <input type="number" className="form-control" id="year" name="year" placeholder="Enter Your Year of Study"/>
+            <input onChange={this.handleChange} value={this.state.value} type="number" className="form-control" id="year" name="year" placeholder="Enter Your Year of Study"/>
             
           </div>
           
         <div className="form-group">
           <label for="exampleInputPassword1">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+          <input onChange={this.handleChange} name="password" value={this.state.value} type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
         </div>
         <div className="form-group">
             
             <label for="confirmPass">Confirm Password</label>
-            <input type="password" className="form-control" id="confirmPass" name="confirmPass" placeholder="Confrim Your Password"/>
+            <input onChange={this.handleChange} value={this.state.value} type="password" className="form-control" id="confirmPass" name="confirmPass" placeholder="Confrim Your Password"/>
             
           </div>
 
