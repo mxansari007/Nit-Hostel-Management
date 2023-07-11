b598f34ad74c09dbf3608a953f75bd2f3318ddf9
deepak sharma <deepaksharma77899@gmail.com>
2023-07-11 14:48:54 +0530

    jsonUpload api created

--- backend/routers/routes.js
+++ backend/routers/routes.js
@@ -1,22 +1,21 @@
 const express =require('express');
 const router =new express.Router();
 const multer  = require('multer');
-const path = require("path");
 
 
 const {studentInfo} =require("../controllers/Admins/studentInfo");
 const {csvUpload} =require("../controllers/Admins/csvUpload");
+const {jsonUpload} = require('../controllers/Admins/jsonUpload');
 const {studentLogin} =require("../controllers/Students/studentLogin");
-const {adminLogin} =require("../controllers/Admins/auth");
+const {adminLogin} =require("../controllers/Admins/adminLogin");
 const {studentSignup} =require("../controllers/Students/studentSignup");
 const {pdfDownload} =require("../controllers/pdf-download");
-const {auth} = require("../middlewares/auth");
 
 
-// console.log("dirname: " + __dirname);
+
 var storage = multer.diskStorage({
     destination:(req,file,cb)=>{
-        cb(null,path.join(__dirname,"../public/uploads"));
+        cb(null,'./backend/public/uploads')
     },
     filename:(req,file,cb)=>{
        cb(null,file.originalname);
@@ -35,12 +34,13 @@ router.post('/adminlogin',adminLogin);
 router.post('/studentlogin',studentLogin);
 
 //// for csv students upload
-router.post('/csv',upload.single('file'), csvUpload);
+router.post('/csv',upload.single('file'), csvUpload,jsonUpload);
+////for json students upload
+router.post('/jsonUpload',jsonUpload);
 //// to download pdf file
 router.post('/pdfDownload',pdfDownload);
 //// view student data
 router.post('/viewStudentInfo',studentInfo);
-//// ftp server
 
 
 
